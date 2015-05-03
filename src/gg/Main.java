5114777b0c9805ca03efe2bdb558e06200283ccc
Lean Rada <godffrey0@gmail.com>
2015-05-03 13:58:56 +0800

    Add Main class

---
+++ src/gg/Main.java
@@ -0,0 +1,16 @@
+package gg;
+
+public class Main {
+
+	public Main() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello world!");
+
+	}
+
+}
