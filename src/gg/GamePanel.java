1523bb03358652dc7b0c4f8e14d37597dd540795
Lean Rada <godffrey0@gmail.com>
2015-05-04 03:09:53 +0800

    GamePanel grid buttons with imagesxdz

--- src/gg/GamePanel.java
+++ src/gg/GamePanel.java
@@ -14,8 +14,28 @@ public class GamePanel extends JPanel {
 	private static final long serialVersionUID = 1L;
 
 	private Engine engine;
+	
+	private JButton grid[][] = new JButton[9][8];
 
 	private Piece selectedPiece = null;
+	
+	private static final ImageIcon images[][] = new ImageIcon[][]{
+		{new ImageIcon("/res/blackpiece0.png"), new ImageIcon("/res/whitepiece0.png")},
+		{new ImageIcon("/res/blackpiece1.png"), new ImageIcon("/res/whitepiece1.png")},
+		{new ImageIcon("/res/blackpiece2.png"), new ImageIcon("/res/whitepiece2.png")},
+		{new ImageIcon("/res/blackpiece3.png"), new ImageIcon("/res/whitepiece3.png")},
+		{new ImageIcon("/res/blackpiece4.png"), new ImageIcon("/res/whitepiece4.png")},
+		{new ImageIcon("/res/blackpiece5.png"), new ImageIcon("/res/whitepiece5.png")},
+		{new ImageIcon("/res/blackpiece6.png"), new ImageIcon("/res/whitepiece6.png")},
+		{new ImageIcon("/res/blackpiece7.png"), new ImageIcon("/res/whitepiece7.png")},
+		{new ImageIcon("/res/blackpiece8.png"), new ImageIcon("/res/whitepiece8.png")},
+		{new ImageIcon("/res/blackpiece9.png"), new ImageIcon("/res/whitepiece9.png")},
+		{new ImageIcon("/res/blackpiece10.png"), new ImageIcon("/res/whitepiece10.png")},
+		{new ImageIcon("/res/blackpiece11.png"), new ImageIcon("/res/whitepiece11.png")},
+		{new ImageIcon("/res/blackpiece12.png"), new ImageIcon("/res/whitepiece12.png")},
+		{new ImageIcon("/res/blackpiece13.png"), new ImageIcon("/res/whitepiece13.png")},
+		{new ImageIcon("/res/blackpiece14.png"), new ImageIcon("/res/whitepiece14.png")}
+	};
 
 	public GamePanel(final MainFrame mainFrame) {
 		super(new FlowLayout(FlowLayout.CENTER, 0, 0));
@@ -45,7 +65,7 @@ public class GamePanel extends JPanel {
 		// Set up grid of buttons
 		for(int x=0; x<9; x++){
 			for(int y=0; y<8; y++){
-				JButton tileButton = new JButton(y + "," + x);
+				JButton tileButton = grid[x][y] = new JButton(y + "," + x);
 				tileButton.setBounds(
 					boardLabel.getBounds().x + 8 + x * 64,
 					boardLabel.getBounds().y + 8 + y * 64
@@ -72,7 +92,7 @@ public class GamePanel extends JPanel {
 	}
 	
 	public JButton PaintPieces(int x, int y, String color, final int rank) {
-		ImageIcon pieceBg = new ImageIcon("res/" + color + "piece" + ".png");
+		ImageIcon pieceBg = images[rank][color == "white" ? 1 : 0];
 		JLabel pieceLabel = new JLabel(pieceBg);
 		ImageIcon pieceIcon = new ImageIcon("res/" + color + "piece" + rank + ".png");
 		JButton pieceButton = new JButton(pieceIcon);
@@ -93,4 +113,18 @@ public class GamePanel extends JPanel {
 		return pieceButton;
 	}
 
+	public void updateGrid(){
+		for(int x=0; x<9; x++){
+			for(int y=0; y<8; y++){
+				Piece piece = engine.board.getPieceAt(y, x);
+				JButton button = grid[x][y];
+				if(piece != null){
+					ImageIcon image = images[piece.getPieceRank()][piece.getTeam() ? 1 : 0];
+					button.setIcon(image);
+				}else{
+					button.setIcon(null);
+				}
+			}
+		}
+	}
 }
