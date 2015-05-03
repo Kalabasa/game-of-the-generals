433f11197dd2ad125d800f7a9b8e8da49660e831
Lean Rada <godffrey0@gmail.com>
2015-05-04 01:29:49 +0800

    Add Engine to GamePanel

--- src/gg/GamePanel.java
+++ src/gg/GamePanel.java
@@ -13,9 +13,13 @@ import javax.swing.JPanel;
 public class GamePanel extends JPanel {
 
 	private static final long serialVersionUID = 1L;
+	
+	private Engine engine;
 
 	public GamePanel(final MainFrame mainFrame) {
 		super(new FlowLayout(FlowLayout.CENTER, 0, 0));
+		
+		engine = new Engine();
 
 		ImageIcon gameBg = new ImageIcon("res/gamebg.png");
 		JLabel gameLabel = new JLabel(gameBg);
